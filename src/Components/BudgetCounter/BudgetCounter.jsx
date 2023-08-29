dc30eabc9a86caa63f922bbd8b58fd49fbb57067
kevin46dsa <kevin0108dsa@gmail.com>
2023-08-29 17:01:12 -0400

    lint

--- src/Components/BudgetCounter/BudgetCounter.jsx
+++ src/Components/BudgetCounter/BudgetCounter.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate,useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { doc,getDoc,updateDoc,arrayUnion, Timestamp} from "firebase/firestore";
 import { db } from "../../firebase"
 import "./BudgetCounter.css"
