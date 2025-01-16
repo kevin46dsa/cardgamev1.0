e64b0c77c3962e0ea45fba94d8e02bf03f0c1fb7
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2025-01-15 19:51:37 -0500

    Update package.json to specify Node.js engine version and clean up imports in PlayerCard component

--- src/Components/MrWhite/Components/PlayerCard.jsx
+++ src/Components/MrWhite/Components/PlayerCard.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { Card } from "react-bootstrap";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import { Modal } from "react-bootstrap";
 
