6de4d4ecfe77e804cbceffee4b11484e3a2e90af
kevin46dsa <kevin0108dsa@gmail.com>
2026-08-20 00:20:46 -0400

    Remove BudgetCounter component and associated styles from the application

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Components/Home/Home';
 import MainPlayer from './Components/MainPlayer/MainPlayer';  
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import BudgetCounter from './Components/BudgetCounter/BudgetCounter';
 import Truthordrinkplayer from './Components/Truthordrink/Truthordrinkplayer';
 import Truthordrinkrandomplayer from './Components/Truthordrink/Truthordrinkrandom';
 import UpdateMenu from './Components/UpdateMenu/UpdateMenu';
@@ -18,7 +17,6 @@ function App() {
                 <Route path='/' element={<Home />} />
                 <Route path="/:id" element={<MainPlayer/>}/>
                 <Route path="/update/:id" element={<UpdateMenu/>}/>
-                <Route path="/budgetcounter/:id" element={<BudgetCounter/>}/>
                 <Route path="/truthordrinkrandom" element={<Truthordrinkrandomplayer/>}/>
                 <Route path="/truthordrink/:id" element={<Truthordrinkplayer/>}/>
                 <Route path="/extra/generatefolder" element={<GenerateFolder />} />
